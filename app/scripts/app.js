1d14bd6eacd66cfd4ea6858526371f180bebd480
Sam Casey <samcaseydesign@gmail.com>
2015-09-18 00:47:42 -0500

    added ui-router script to index, added ui.router dependency to angular module, added blocJams config

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,14 @@
-var blocJamsMod = angular.module('blocJams', []);
+var blocJams = angular.module('blocJams', ['ui.router']);
+
+blocJams.config(function($stateProvider, $locationProvider){
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+    $stateProvider.state('album', {
+        url: '/album',
+        controller: 'Album.controller',
+        templateUrl: '/templates/album.html'
+    });
+});
+
